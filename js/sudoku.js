d6939d1fb4050fdd0e936c49f929be1a055e196f
Nikhil Kumar <91065041+nikhilkumar99k@users.noreply.github.com>
2022-08-05 01:51:29 +0530

    update js

--- js/sudoku.js
+++ js/sudoku.js
@@ -8,13 +8,27 @@ const b = null
                   [[2,1],[4,3],[6,9],[7,5],[8,4],[9,7],[10,4],[13,1],[22,2],[25,6],[26,9],[31,4],[34,8],[35,7],[39,5],[45,4],[47,7],[50,8],[51,2],[54,5],[55,5],[56,3],[57,1],[60,7],[63,9],[64,6],[79,3],[80,1]]]
 
     var arrHard = [[[1,8],[11,3],[12,6],[20,7],[23,9],[25,2],[29,5],[33,7],[41,4],[42,5],[43,7],[49,1],[53,3],[57,1],[62,6],[63,8],[66,8],[67,5],[71,1],[74,9],[40,8],[79,4]],
-                  [[3,5],[4,3],[10,8],[17,2],[20,7],[23,1],[25,5],[28,4],[33,5],[34,3],[38,1],[41,7],[45,6],[48,3],[49,2],[53,8],[56,6],[58,5],[63,9],[66,4],[71,3],[78,9],[79,7]]]
+                  [[3,5],[4,3],[10,8],[17,2],[20,7],[23,1],[25,5],[28,4],[33,5],[34,3],[38,1],[41,7],[45,6],[48,3],[49,2],[53,8],[56,6],[58,5],[63,9],[66,4],[71,3],[78,9],[79,7]],
+                  [[1,1],[6,7],[8,9],[11,3],[14,2],[18,8],[21,9],[22,6],[25,5],[30,5],[31,3],[34,9],[38,1],[41,8],[45,2],[46,6],[51,4],[55,3],[62,1],[65,4],[72,7],[75,7],[79,3]]]
 
+    var arrEvil = [[[1,8],[2,5],[6,2],[7,4],[10,7],[11,2],[18,9],[21,4],[31,1],[33,7],[36,2],[37,3],[39,5],[43,9],[47,4],[59,8],[62,7],[65,1],[66,7],[77,3],[78,6],[80,4]],
+                   [[1,8],[5,7],[11,7],[12,5],[13,3],[15,8],[19,6],[21,9],[23,1],[33,1],[34,4],[36,8],[48,7],[52,3],[54,2],[55,2],[56,4],[61,5],[64,9],[67,4],[69,7],[70,6],[77,3],[78,6]]]
+
+    var arrDanger =   [[[4,7],[10,1],[22,4],[23,3],[25,2],[36,6],[40,5],[42,9],[52,4],[53,1],[54,8],[59,8],[60,1],[66,2],[71,5],[74,4],[79,3]],
+                      [[1,9],[4,8],[16,5],[29,2],[32,1],[36,3],[38,1],[44,6],[49,4],[53,7],[55,7],[57,8],[58,6],[68,3],[70,1],[73,4],[79,2]]]
+  var ex=0;
+  var hx=0;
+  var mx=0;
+  var elx=0;
+  var dx=0;
 function easy(){
   for (var i = 1; i <= 81; i++) {
     document.getElementById(String(i)).value = ''
   }
-var x = Math.floor((Math.random() * arrEasy.length));
+// var x = Math.floor((Math.random() * arrEasy.length));
+let x=ex%arrEasy.length;
+ex++;
+if(ex>100) ex=0;
 var a=arrEasy[x];
   for (var i = 0; i < a.length; i++) {
     document.getElementById(String(a[i][0])).value = (a[i][1])
@@ -25,7 +39,10 @@ function medium(){
   for (var i = 1; i <= 81; i++) {
     document.getElementById(String(i)).value = ''
   }
-var x = Math.floor((Math.random() * arrMedium.length));
+// var x = Math.floor((Math.random() * arrMedium.length));
+let x=mx%arrMedium.length;
+mx++;
+if(mx>100) mx=0;
 var a=arrMedium[x];
   for (var i = 0; i < a.length; i++) {
     document.getElementById(String(a[i][0])).value = (a[i][1])
@@ -37,13 +54,42 @@ function hard(){
   for (var i = 1; i <= 81; i++) {
     document.getElementById(String(i)).value = ''
   }
-let x = Math.floor((Math.random() * arrHard.length));
+// let x = Math.floor((Math.random() * arrHard.length));
+let x=hx%arrHard.length;
+hx++;
+if(hx>100) hx=0;
 var a=arrHard[x];
   for (var i = 0; i < a.length; i++) {
     document.getElementById(String(a[i][0])).value = (a[i][1])
   }
 }
 
+function evil(){
+  for (var i = 1; i <= 81; i++) {
+    document.getElementById(String(i)).value = ''
+  }
+// let x = Math.floor((Math.random() * arrEvil.length));
+let x=elx%arrEvil.length;
+elx++;
+if(elx>100) elx=0;
+var a=arrEvil[x];
+  for (var i = 0; i < a.length; i++) {
+    document.getElementById(String(a[i][0])).value = (a[i][1])
+  }
+}
+function danger(){
+  for (var i = 1; i <= 81; i++) {
+    document.getElementById(String(i)).value = ''
+  }
+// let x = Math.floor((Math.random() * arrDanger.length));
+let x=dx%arrDanger.length;
+dx++;
+if(dx>100) dx=0;
+var a=arrDanger[x];
+  for (var i = 0; i < a.length; i++) {
+    document.getElementById(String(a[i][0])).value = (a[i][1])
+  }
+}
 
 //
 function initiate() {
